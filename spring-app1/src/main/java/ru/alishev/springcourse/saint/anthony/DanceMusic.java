e6ae4734ef797c958c6ce0989ce6c270bb10fc39
Toxa2202 <svyatoj@gmail.com>
2019-09-27 21:17:08 +0300

    Lesson 4 alishev spring

---
+++ spring-app1/src/main/java/ru/alishev/springcourse/saint/anthony/DanceMusic.java
@@ -0,0 +1,8 @@
+package ru.alishev.springcourse.saint.anthony;
+
+public class DanceMusic implements Music {
+    @Override
+    public String getSong() {
+        return "Exploration to the space.";
+    }
+}
