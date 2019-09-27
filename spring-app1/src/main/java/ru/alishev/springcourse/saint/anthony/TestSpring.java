94893adddb2524902b5b8fa3bf6e362d368f76d3
Toxa2202 <svyatoj@gmail.com>
2019-09-27 22:38:20 +0300

    Lesson 7 alishev spring. Scope prototype

--- spring-app1/src/main/java/ru/alishev/springcourse/saint/anthony/TestSpring.java
+++ spring-app1/src/main/java/ru/alishev/springcourse/saint/anthony/TestSpring.java
@@ -9,8 +9,21 @@ public class TestSpring {
 
 //        Music music = context.getBean("musicBean", Music.class);
 //        MusicPlayer musicPlayer = new MusicPlayer(music);
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        musicPlayer.playMusic();
+        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+
+        boolean comparison = firstMusicPlayer == secondMusicPlayer;
+
+        System.out.println(comparison);
+
+        System.out.println(firstMusicPlayer);
+        System.out.println(secondMusicPlayer);
+
+        firstMusicPlayer.setVolume(10);
+        System.out.println(firstMusicPlayer.getVolume());
+        System.out.println(secondMusicPlayer.getVolume());
+
+//        musicPlayer.playMusic();
 
         context.close();
     }
